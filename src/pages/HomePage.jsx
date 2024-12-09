9d90e401d9a8ca8ee8e18f37a17b0f4bcf0e4e88
emran <emran-93@outlook.com>
2024-12-09 21:09:00 +0300

    update structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Profile from "../components/Profile"
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Profile />
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Login from "../components/Login"
+
+function LoginPage() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <Login />
+        </div>
+    )
+}
+
+export default LoginPage
