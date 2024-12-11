a64912ce5bad5ea03c0712b5c6ab47e349066c0b
Emran <emran.marei@gmail.com>
2024-12-11 12:56:45 +0300

    u

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,26 @@
+/* 
+    form
+    username, password, submit button
+    onSubmit => get JWT and store it in local storage
+*/
+
 function Login() {
     return (
-        <>
-            <h1>Login ...</h1>
-        </>
+        <form className="form--main">
+            <input 
+                type="text" 
+                placeholder="Username" 
+                id="username"
+                value={uname}
+                onChange={}
+            />
+            <input 
+                type="text" 
+                placeholder="Password" 
+                id="password"
+            />
+            <button type="submit">Login</button>
+        </form>
     )
 }
 
